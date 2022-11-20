697636de9587ca9ca98903690ff9dda16c4ee413
kwonmoto <spadekwon@gmail.com>
2022-11-21 00:27:38 +0900

    디렉토리 및 내부 마크다운 파일 생성

---
+++ src/pages/PageLogin.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import React from "react";
+
+export default function PageLogin() {
+  return <div>로그인페이지</div>;
+}
---
+++ src/styles/pages.md
@@ -0,0 +1,7 @@
+# Styles
+
+styled-component 파일 혹은 css파일들을 모아둔 폴더
+
+## Rules
+
+폴더 내에 또 다른 폴더를 생성하지 마세요.
